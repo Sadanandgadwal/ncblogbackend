d8c0cde280d201f5c42ab56d1adbbbdd61d5f85b
Sadanand_gadwal <83832977+Sadanandgadwal@users.noreply.github.com>
2023-08-10 05:00:15 +0530

    jwt.ts

--- src/utils/jwtValidate.ts
+++ src/utils/jwtValidate.ts
@@ -1,4 +1,4 @@
-import responses from "./responses.js";
+import responses from "./responses.ts";
 import jwt from "jsonwebtoken";
 import {NextFunction, Request, Response} from "express";
 import User from "../models/user.model";
